1a3f2f501101306d0c03376a685f799ff0b77746
Boscobrand <Tony.Bosco@boscobrand.com>
2022-03-29 15:52:55 -0400

    friends don't let friends not commit

--- squad.js
+++ squad.js
@@ -14,9 +14,19 @@ function beginGame() {
 //game pattern generator(TBD)
 let levelCount=0
 
+function sequenceGenerator() {
+    const touchdata = ['red1','wht1','gra1','grn1']
+    const generate = touchdata[Math.floor(Math.random() * touchdata.length)]
+    //Multiplying touchdata.length and Math.random creates a range between 1-4 instead of floats between 0-1. Math floor rounds fractional numbers down to whole numbers (0-3) 
+    return random;
+}
+
+
 function advanceRound() {
-    level +=1;
+    levelCount +=1;
     const advSimonArray =[...SimonArray];
+    //spread operator: p.196 8.3.4 O'Reilly Javascript The Definitive Guide 7th Ed, by David Flanagan
+    advSimonArray.push(advanceRound)
 }
 
 
