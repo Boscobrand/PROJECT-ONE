092113685335159597909922d4343e2240817483
Boscobrand <Tony.Bosco@boscobrand.com>
2022-03-29 10:29:48 -0400

    JS commented dev steps

--- squad.js
+++ squad.js
@@ -1,2 +1,27 @@
+//Set up arrays to collect data patterns
+
 let simonarray = []
 let playerarray = []
+
+//start of game
+
+
+//tile activation & animation
+
+
+//game pattern generator
+
+
+//computer turn
+
+
+//player turn
+
+
+//tracking
+
+
+//status window updates
+
+
+//
