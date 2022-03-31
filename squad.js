001f094f1c4b142f991e621763516ae6b5477e43
Boscobrand <Tony.Bosco@boscobrand.com>
2022-03-30 22:21:47 -0400

    JS current review w/o commlink

--- squad.js
+++ squad.js
@@ -1,56 +1,56 @@
 //Set up arrays to collect data patterns
+let computerArray = [];
 let playerArray = [];
-let simonArray = [];
 
 //setup
 const start = document.getElementById('defuse');
 //const text = document.querySelector('text')
 const readout = document.querySelector('.readout1');
-
 const readout2 = document.querySelector('.readout2');
+const touchpad_set=document.querySelector('.touchpad_set');
 
 let levelCount = 0;
 
 function reset(text) {
     alert(text);
-    simonArray=[];
+    computerArray=[];
     playerArray=[];
     levelCount = 0;
     readout.textContent = "PRESS DEFUSE TO BEGIN";
-    //2
+    //note 2
 }
 
-//
+//note 2.5
 function playerTurn(levelCount){
-    readout.textContent = `YOUR TURN: $(levelCount) Entries$(levelCount>1 ?'s':")`
+    readout.textContent = `YOUR TURN: ${levelCount} Entries${levelCount > 1 ? 's': ''}`;
 }
 
 //touchpad activation
-function touchpadAction(touchdata) {
-    const touched = document.querySelector(`[touchdata='${touchdata}']`);
-    //const signal = document.querySelector(`[touchsignal = '${touchdata}']`);
+function touchpadAction(color) {
+    const touched = document.querySelector(`[data-touch='${color}']`);
+    const sfx = document.querySelector(`[data-sfx = '${color}']`);
 
     touched.classList.add('pressed');
-    //signal.play();
+    sfx.play();
 
     setTimeout(() => {
         touched.classList.remove('pressed');
-    }, 300);
+    }, 350);
 }
 
-//This function will iterate over the advSimonArray, and stall the iteration so all values arent called at once, turning on all touchpads at the same tine.
-function play(advSimonArray) {
-    advSimonArray.forEach((touchdata,index) => {
+//This function will iterate over the advcomputerArray, and stall the iteration so all values arent called at once, turning on all touchpads at the same tine.
+function play(advcomputerArray) {
+    advcomputerArray.forEach((color,index) => {
         setTimeout(() => {
-            touchpadAction(touchdata);
+            touchpadAction(color);
         }, (index + 1) * 600);
     });
 }
 
 function sequenceGenerator() {
-    const touchdata = ['red1','white1','gray1','green1'];
-    const generate = touchdata[Math.floor(Math.random() * touchdata.length)];
-    //Multiplying touchdata.length and Math.random creates a range between 1-4 instead of floats between 0-1. Math floor rounds fractional numbers down to whole numbers (0-3) 
+    const touchpads = ['red1','white1','gray1','green1'];
+    const generate = touchpads[Math.floor(Math.random() * touchpads.length)];
+    //Multiplying data-touch.length and Math.random creates a range between 1-4 instead of floats between 0-1. Math floor rounds fractional numbers down to whole numbers (0-3) 
     //referenced: https://discuss.codecademy.com/t/how-do-math-random-and-math-floor-work-together/490890
     return generate;
 }
@@ -59,35 +59,37 @@ function advanceRound() {
     levelCount +=1;
 
     readout.textContent = "REMEMBER THIS SEQUENCE";
-    readout2.textContent= `LEVEL $(levelCount) of 10`;
+    readout2.textContent= `LEVEL ${levelCount} of 15`;
 
-    const advSimonArray =[...simonArray];
+    const advcomputerArray =[...computerArray];
     //spread operator: p.196 8.3.4 O'Reilly Javascript The Definitive Guide 7th Ed, by David Flanagan
-    advSimonArray.push(sequenceGenerator));
-    play(advSimonArray);
+    advcomputerArray.push(sequenceGenerator);
+    //console.log(advcomputerArray);
+    play(advcomputerArray);
 
-    simonArray = [...advSimonArray];
+    computerArray = [...advcomputerArray];
     setTimeout(() =>{
         playerTurn(levelCount);
-    }, levelCount * 600 + 1000);
+    }, levelCount * 550 + 1100);
 }
+
+const remainingEntries = computerArray.length - playerArray.length;
 
 function capture(touchpad) {
     const index = playerArray.push(touchpad)-1;
-    //signalplay
+    const sfx = document.querySelector(`[data-sfx='${touchpad}']`);
+    sfx.play()
     
-    const remaining = simonArray.length - playerArray.length;
-
-    if(playerArray[index] !== simonArray[index]) {
+    if(playerArray[index] !== computerArray[index]) {
         //play exploding gif
         reset('TICK...TICK...BOOM! GAME OVER!');
         return;
     }
 
-    if(playerArray.length === simonArray.length) {
-        if (playerArray.length === 14);
+    if(playerArray.length === computerArray.length) {
+        if (playerArray.length === 15);
         //play applause gif
-        reset('TICK...TICK...CLICK! NICE WORK, BOMB DEFUSED!');
+       reset('TICK...TICK...CLICK! NICE WORK, BOMB DEFUSED!');
         return;
     }
 
@@ -95,15 +97,14 @@ function capture(touchpad) {
     readout.textContent = "GOOD, STAY FOCUSED!";
     setTimeout(() => {
         advanceRound();
-    }, 1000);
+    }, 1100);
     return;
-
-    readout2.textContent = `GO! ${remaining} Entries${
-        remaining >1 ? 's' : ''
-        }`;
 }
+    readout2.textContent = `GO! ${remainingEntries} Entries${
+        remainingEntries >1 ? 's' : ''
+    }`;
 
-
+    
 
 function begin() {
     readout.textContext = "REMEMBER THIS SEQUENCE";
@@ -112,7 +113,7 @@ function begin() {
 
 start.addEventListener('click', begin);
 
-touchpad.addEventListener('click',event => {
-    const[touchpad] = event.target.touchdata;
-    //if (xxx) capture( //xxxx );
+touchpad_set.addEventListener('click', event => {
+    const[touchpad] = event.target.dataset;
+    if (touchpad) capture(touchpad);
 });
